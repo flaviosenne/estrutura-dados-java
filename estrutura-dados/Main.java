08ea6ce31fc1f001cc974461041388d228d1b0f9
Joao Flavio <flaviosenne123@gmail.com>
2021-07-02 19:35:58 -0300

    heap sort

--- estrutura-dados/Main.java
+++ estrutura-dados/Main.java
@@ -9,7 +9,8 @@ public class Main {
         // buscaBinaria();
         // bubbleSort();
         // insertionSort();
-        selectionSort();
+        // selectionSort();
+        heapSort();
     }
 
     private static void array() {
@@ -237,6 +238,64 @@ public class Main {
             System.out.println(vetor[i]);
         }
 
+    }
+
+    private static void heapSort() {
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) Math.floor(Math.random() * vetor.length);
+        }
+
+        System.out.print("desordenado ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+
+        int n = vetor.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            aplicarHeap(vetor, n, i);
+        }
+
+        System.out.print("quase ordenado ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+
+        for (int j = n - 1; j > 0; j--) {
+            int aux = vetor[0];
+            vetor[0] = vetor[j];
+            vetor[j] = aux;
+
+            aplicarHeap(vetor, j, 0);
+        }
+
+        System.out.print("ordenado ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+
+    private static void aplicarHeap(int[] vetor, int n, int i) {
+        int raiz = i;
+        int esquerda = 2 * i + 1;
+        int direita = 2 * i + 2;
+
+        if (esquerda < n && vetor[esquerda] > vetor[raiz]) {
+            raiz = esquerda;
+        }
+        if (direita < n && vetor[direita] > vetor[raiz]) {
+            raiz = direita;
+        }
+
+        if (raiz != i) {
+            int aux = vetor[i];
+            vetor[i] = vetor[raiz];
+            vetor[raiz] = aux;
+
+            aplicarHeap(vetor, n, raiz);
+        }
     }
 
 }
